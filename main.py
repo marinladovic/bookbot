03369a8990d1493f0b7e2eb3f0fb2b902757a917
marin.ladovic <marin.ladovic@bozic.io>
2024-08-21 17:34:52 +0200

    get word count

---
+++ main.py
@@ -0,0 +1,14 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    print(num_words)
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def get_num_words(contents):
+    return len(contents.split())
+
+main()

8ed399e3d3327e6cc2f689592535e7fc829f5bcb
marin.ladovic <marin.ladovic@bozic.io>
2024-08-21 17:41:53 +0200

    get character count

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    num_words = get_num_words(text)
-    print(num_words)
+    print(get_character_count(text))
 
 def get_book_text(path):
     with open(path) as f:
@@ -10,5 +9,15 @@ def get_book_text(path):
 
 def get_num_words(contents):
     return len(contents.split())
+
+def get_character_count(text):
+    lowered_string = text.lower()
+    char_count_dict = {}
+    for char in lowered_string:
+        if char in char_count_dict:
+            char_count_dict[char] += 1
+        else:
+            char_count_dict[char] = 1
+    return char_count_dict
 
 main()

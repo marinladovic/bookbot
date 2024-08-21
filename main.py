6421822276c48328763242bfde42ef0631223c9d
marin.ladovic <marin.ladovic@bozic.io>
2024-08-21 18:01:58 +0200

    print report

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(get_character_count(text))
+    num_words = get_num_words(text)
+    chars_dict = get_character_count(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print_report(book_path, num_words, chars_sorted_list)
 
 def get_book_text(path):
     with open(path) as f:
@@ -19,5 +22,27 @@ def get_character_count(text):
         else:
             char_count_dict[char] = 1
     return char_count_dict
+    
+def sort_on(d):
+    return d["num"]
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for char in num_chars_dict:
+        sorted_list.append({"char": char, "num": num_chars_dict[char]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+def print_report(book_path, num_words, chars_list):
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    print()
+
+    for item in chars_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
 
 main()
